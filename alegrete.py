f1d6d07bb1ab4b67cf2dc1933e8a0567a8b0fd84
Lucas Gomes de Souza <lucasgsouza2013@gmail.com>
2025-09-17 22:30:01 -0300

    primeiro update das funções do alegrete.py
    primeira implementação das funções (não otimizadas) do erro quadrático médio, gradiente descendente e iteração das épocas.

--- alegrete.py
+++ alegrete.py
@@ -10,8 +10,20 @@ def compute_mse(b, w, data):
     :return: float - o erro quadratico medio
     """
 
-    help(sum);
-    raise NotImplementedError  # substituir pelo seu codigo
+    n = len(data)   # número de observações
+    total_error = 0
+    
+    for i in range(n):
+        x = data[i][0]  # área
+        y = data[i][1]  # preço real
+        
+        y_pred = b + w * x   # valor previsto pela regressão
+        error = (y - y_pred) ** 2  # erro quadrático dessa entrada
+        
+        total_error += error  # acumula os erros
+    
+    mse = total_error / n  # média dos erros quadráticos
+    return mse
 
 
 def step_gradient(b, w, data, alpha):
@@ -23,7 +35,29 @@ def step_gradient(b, w, data, alpha):
     :param alpha: float - taxa de aprendizado (a.k.a. tamanho do passo)
     :return: float,float - os novos valores de b e w, respectivamente
     """
-    raise NotImplementedError  # substituir pelo seu codigo
+    n = len(data)
+    b_gradient = 0
+    w_gradient = 0
+    
+    # calcula os gradientes somando contribuição de cada ponto
+    for i in range(n):
+        x = data[i][0]
+        y = data[i][1]
+        y_pred = b + w * x
+        error = y - y_pred
+        
+        b_gradient += -2 * error
+        w_gradient += -2 * x * error
+    
+    # tira a média
+    b_gradient /= n
+    w_gradient /= n
+    
+    # atualiza parâmetros
+    b = b - alpha * b_gradient
+    w = w - alpha * w_gradient
+    
+    return b, w
 
 
 def fit(data, b, w, alpha, num_iterations):
@@ -41,4 +75,16 @@ def fit(data, b, w, alpha, num_iterations):
     :param num_iterations: int - numero de épocas/iterações para executar a descida de gradiente
     :return: list,list - uma lista com os b e outra com os w obtidos ao longo da execução
     """
-    raise NotImplementedError  # substituir pelo seu codigo
+     # listas para guardar os valores de b e w a cada época
+    b_history = [b]
+    w_history = [w]
+    
+    for i in range(num_iterations):
+        # atualiza b e w usando uma iteração da descida do gradiente
+        b, w = step_gradient(b, w, data, alpha)
+        
+        # guarda os novos valores
+        b_history.append(b)
+        w_history.append(w)
+    
+    return b_history, w_history
